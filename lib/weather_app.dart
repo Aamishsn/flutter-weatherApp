ccd01934eaf72bfc5c0f63480457882fda1a3700
Aamishsn <aamishsn@gmail.com>
2024-11-03 20:09:40 +0500

    31124-2

--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -5,31 +5,86 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-       appBar: PreferredSize(
-     preferredSize: const Size.fromHeight(120.0), // Set the desired height here
-    child: AppBar(
-      title: const Text("Weathe App",
-      style: TextStyle(fontSize: 32,
-      fontWeight: FontWeight.bold,
-
-      ),
-      ),
-      centerTitle: true,
-      backgroundColor: Colors.blue,
-      actions: [
-        IconButton(
-          padding: const EdgeInsets.only(right: 10),
-          iconSize: 42,
-          icon: const Icon(Icons.refresh),
-          onPressed: () {
-            print('refresh button pressed');
-          },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weathe App",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      ],
-    ),
-  ),
-
+        centerTitle: true,
+        // backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+            padding: const EdgeInsets.only(right: 10),
+            iconSize: 42,
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              // print('refresh button pressed');
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                // height: 250,
+                width: double.infinity,
+                child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    elevation: 10,
+                    child: const Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(children: [
+                        Text(
+                          '300 K',
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 64,
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          'Rain',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ]),
+                    )),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              const Text(
+                "Weather Forcast",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
